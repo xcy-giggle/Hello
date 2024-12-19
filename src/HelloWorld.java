947c207f382a732b02aca961395995950e2f2c35
xcy-giggle <3396002414@qq.com>
2024-12-19 15:49:18 +0800

    李四开发新功能

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,4 +2,8 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("hello Lisi");
     }
+
+   void newTask(){
+        System.out.println("新功能");
+   }
 }
